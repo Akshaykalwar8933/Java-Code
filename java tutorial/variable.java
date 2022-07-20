a47ff56f1fb9359ebf5a5e07f3fa0316d60c451a
Akshaykalwar8933 <106814495+Akshaykalwar8933@users.noreply.github.com>
2022-07-21 00:08:06 +0530

    Code

---
+++ java tutorial/variable.java
@@ -0,0 +1,10 @@
+import javax.annotation.processing.SupportedOptions;
+
+class variable{
+    public static void main(String[]args){
+        int a = 6;
+        int b = 9;
+        int value = a * b / a - b ;
+    System.out.println(value);    
+}
+}
