09cfbc1640f1b13317556e715814724207a1ed90
Akshaykalwar8933 <106814495+Akshaykalwar8933@users.noreply.github.com>
2022-07-22 00:54:04 +0530

    Add files via upload

---
+++ SolveJavaProblem/robot.java
@@ -0,0 +1,25 @@
+package SolveJavaProblem;
+
+import java.util.*;
+public class robot {
+    public static void main(String[]args){
+    Scanner sc = new Scanner (System.in);
+    int button = sc.nextInt();
+    if(button == 1){
+
+        System.out.println("hello");
+    }else if(button == 2){
+        System.out.println("good morning");
+    }else if(button == 3){
+        System.out.println("akshay");
+    }else {
+        System.out.println("invalid");
+    }
+   
+    
+}
+}
+
+    
+
+ 
